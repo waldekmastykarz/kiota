7cdc20c21e18123774b2359e299213071257a076
Andrew Omondi <andrew.omondi@microsoft.com>
2021-08-23 14:51:17 +0300

    WIP

---
+++ http/dotnet/httpclient/Microsoft.Kiota.Http.HttpClient.Tests/HttpClientBuilderTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace Microsoft.Kiota.Http.HttpClient.Tests
+{
+    public class HttpClientBuilderTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
